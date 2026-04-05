94dc04ed57a3128e9c8053d38fcd0abe90616d42
Aldwin <magpantay01258@gmail.com>
2026-04-05 17:46:04 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -14,51 +14,51 @@ CREATE DATABASE datamodeling;
 
 DROP SCHEMA IF EXISTS bronze;
 
-DROP TABLE IF EXISTS bronze.src_customer;
-CREATE TABLE src_customer (
+DROP TABLE IF EXISTS bronze.src_customer CASCADE;
+CREATE TABLE bronze.src_customer (
 	customer_id VARCHAR(50) PRIMARY KEY,
-	customer_code INT,
+	customer_code BIGINT,
 	customer_city VARCHAR(100),
 	customer_state VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS bronze.src_orderitems;
-CREATE TABLE src_orderitems (
-	order_id VARCHAR(50),
-	product_id VARCHAR(50),
-	seller_id VARCHAR(50),
-	price DECIMAL(10,2),
-	shipping_charges DECIMAL(10,2)
-	PRIMARY KEY (order_id, product_id),
-	FOREIGN KEY (order_id) REFERENCES src_orders(order_id),
-	FOREIGN KEY (product_id) REFERENCES src_products(product_id)
+DROP TABLE IF EXISTS bronze.src_products CASCADE;
+CREATE TABLE bronze.src_products (
+	product_id VARCHAR(50) PRIMARY KEY,
+	product_category VARCHAR(255),
+	product_weigth NUMERIC,
+	product_length NUMERIC,
+	product_heigth NUMERIC,
+	product_width NUMERIC
 );
 
-DROP TABLE IF EXISTS bronze.src_orders;
-CREATE TABLE src_orders (
+DROP TABLE IF EXISTS bronze.src_orders CASCADE;
+CREATE TABLE bronze.src_orders (
 	order_id VARCHAR(50) PRIMARY KEY,
-	customer_id VARCHAR(50) REFERENCES src_customer(customer_id),
+	customer_id VARCHAR(50) REFERENCES bronze.src_customer(customer_id),
 	order_purchase_timestamp TIMESTAMP,
 	order_approved TIMESTAMP
 );
 
-DROP TABLE IF EXISTS bronze.src_payments;
-CREATE TABLE src_payments (
+DROP TABLE IF EXISTS bronze.src_payments CASCADE;
+CREATE TABLE bronze.src_payments (
 	order_id VARCHAR(50),
 	payment_sequential INT,
 	payment_type VARCHAR(50),
 	payment_installments INT,
-	payment_value DECIMAL(10,2),
+	payment_value NUMERIC(10,2),
 	PRIMARY KEY (order_id, payment_sequential),
-	FOREIGN KEY (order_id) REFERENCES src_orders(order_id)
+	FOREIGN KEY (order_id) REFERENCES bronze.src_orders(order_id)
 );
 
-DROP TABLE IF EXISTS bronze.src_products;
-CREATE TABLE src_products (
-	product_id VARCHAR(50) PRIMARY KEY,
-	product_category TEXT,
-	product_weigth INT,
-	product_length INT,
-	product_heigth INT,
-	product_width INT
+DROP TABLE IF EXISTS bronze.src_orderitems CASCADE;
+CREATE TABLE bronze.src_orderitems (
+	order_id VARCHAR(50),
+	product_id VARCHAR(50),
+	seller_id VARCHAR(50),
+	price NUMERIC(10,2),
+	shipping_charges NUMERIC(10,2),
+	PRIMARY KEY (order_id, product_id),
+	FOREIGN KEY (order_id) REFERENCES bronze.src_orders(order_id),
+	FOREIGN KEY (product_id) REFERENCES bronze.src_products(product_id)
 );
